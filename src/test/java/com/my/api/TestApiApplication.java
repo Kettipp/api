701db46d45bd90cda7d52575c3a94fd84caca631
Pasechna Katerina <pasech.kate@gmail.com>
2024-05-04 12:47:00 +0300

    Initial commit

---
+++ src/test/java/com/my/api/TestApiApplication.java
@@ -0,0 +1,13 @@
+package com.my.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ApiApplication::main).with(TestApiApplication.class).run(args);
+	}
+
+}
